445bbd584ba1be15995973b33a1a5d28bcc6fa07
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2025-05-10 17:52:12 +0200

    Slot Machine initialised and filled with random numbers

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -4,6 +4,28 @@ class SlotMachine
 {
     static void Main()
     {
-        Console.WriteLine("Hello SlotMachine");
+        int numberOfRows = 3;
+        int numberOfColumns = 3;
+        int[,] grid = new int[numberOfRows, numberOfColumns];
+        Random rnd = new Random();
+
+        // Start the Slot Machine
+        for (int row = 0; row <numberOfRows ; row++)
+        {
+            for (int col = 0; col < numberOfColumns; col++)
+            {
+                grid[row, col] = rnd.Next(1, 5); 
+            }
+        }
+        
+        Console.WriteLine("Slot Machine Ergebnis:");
+        for (int row = 0; row < numberOfRows; row++)
+        {
+            for (int col = 0; col < numberOfColumns; col++)
+            {
+                Console.Write(grid[row, col] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
