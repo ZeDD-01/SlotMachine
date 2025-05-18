939dabeb24b769d4e506beae9840218b53e610f0
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2025-05-18 22:47:14 +0200

    New features: 1.Place bet, 2. Decide which horizontal line to play, 3. Add profit if the numbers are correct.

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -8,6 +8,15 @@ class SlotMachine
         int numberOfColumns = 3;
         int[,] grid = new int[numberOfRows, numberOfColumns];
         Random rnd = new Random();
+        
+        Console.Write("Please place your bet ");
+        int bet = int.Parse(Console.ReadLine());
+        
+        Console.Write("Which lines do you want to play?\n");
+        Console.WriteLine("1 - Middle horizontal line");
+        Console.WriteLine("2 - All horizontal lines");
+        Console.Write("Your choice: ");
+        int lineChoice = int.Parse(Console.ReadLine());
 
         // Start the Slot Machine
         for (int row = 0; row <numberOfRows ; row++)
@@ -15,10 +24,12 @@ class SlotMachine
             for (int col = 0; col < numberOfColumns; col++)
             {
                 grid[row, col] = rnd.Next(1, 5); 
+                //With this line below you can test what happens if the user wins.
+                //grid[row, col] = 1; 
             }
         }
         
-        Console.WriteLine("Slot Machine Ergebnis:");
+        Console.WriteLine("Slot Machine Result:");
         for (int row = 0; row < numberOfRows; row++)
         {
             for (int col = 0; col < numberOfColumns; col++)
@@ -27,5 +38,35 @@ class SlotMachine
             }
             Console.WriteLine();
         }
+
+        int profit = 0;
+
+        if (lineChoice == 1)
+        {
+            if (grid[1, 0] == grid[1, 1] && grid[1, 1] == grid[1, 2])
+            {
+                profit +=1;
+            }
+        }
+        else if (lineChoice == 2)
+        {
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                if (grid[row, 0] == grid[row, 1] && grid[row, 1] == grid[row, 2])
+                {
+                    profit += 1;
+                }
+            }
+        }
+        
+        int amountOfMoney = 0;
+
+        if (profit != 0)
+        {
+            amountOfMoney = bet + profit;
+        }
+        
+        Console.WriteLine("\nProfit: $" + profit);
+        Console.WriteLine("Amount of Money: $" + amountOfMoney);
     }
 }
